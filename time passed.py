7a565c78fa95bf53702c49824a60189e244cda09
Sochackii <67477635+Sochackii@users.noreply.github.com>
2020-06-26 19:26:05 +0200

    Update time passed.py

--- time passed.py
+++ time passed.py
@@ -1,8 +1,11 @@
 import time
-class KC():
+
+class KC:
+    
     def __init__(self):
-        self.czas = time.clock()
+        self.czas = time.clock_gettime(time.CLOCK_MONOTONIC_RAW)
+        
     def dlugosc(self):
-        self.czas2= time.clock()
+        self.czas2= time.clock_gettime(time.CLOCK_MONOTONIC_RAW)
         print(self.czas2-self.czas)
         self.czas = self.czas2
